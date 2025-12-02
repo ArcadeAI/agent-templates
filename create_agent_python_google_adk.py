bae480dcebbe085129f1e75d7353158148da9c03
Mateo Torres <torresmateo@gmail.com>
2025-12-02 10:39:10 -0600

    better google adk template

--- create_agent_python_google_adk.py
+++ create_agent_python_google_adk.py
@@ -2,7 +2,7 @@ from pathlib import Path
 from render_utils import create_agent
 
 template_dir = Path(__file__).parent / "python_google_adk"
-output_dir = Path(__file__).parent / "rendered_agent"
+output_dir = Path(__file__).parent / "real_agents"
 
 configuration = {
     "arcade_tool_list": [
